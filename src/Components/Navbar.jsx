72d40781e817e4dc0a901993f75882ed8aadb7d5
Shakhawat-pro <ratulhossain2692004@gmail.com>
2024-08-16 22:34:54 +0600

    nav link

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -6,14 +6,16 @@ import { authContext } from '../Context/AuthProvider';
 
 const Navbar = () => {
     const { user, logOut } = useContext(authContext)
-    
+
 
     return (
         <div className="flex justify-between lg:justify-end gap-2 border-b-2 py-2 lg:pr-3">
-            <label htmlFor="my-drawer-2" className="btn btn-ghost text-3xl drawer-button  ">
-                <IoMdMenu className='lg:hidden' />
-                <img src={logoImg} className='w-12' />
-            </label>
+            <div className='flex items-center'>
+                <label htmlFor="my-drawer-2" className="btn btn-ghost text-3xl drawer-button  lg:hidden">
+                    <IoMdMenu className='' />
+                </label>
+                <img src={logoImg} className='w-12 transform transition-transform hover:scale-110 hover:cursor-pointer'/>
+            </div>
             <div className="form-control max-sm:w-36">
                 <input type="text" placeholder="Search" className="input input-bordered w-96  max-md:w-auto rounded-full" />
             </div>
